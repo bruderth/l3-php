a231751ca98e5e31134af7ad0c08ddf6a413be70
Thomas <thomas.brdr@gmail.com>
2021-07-08 12:26:47 +0200

    :sparkles: Ajout de la possibilité d'ajouter un produit

--- htdocs/src/Controller/CatalogController.php
+++ htdocs/src/Controller/CatalogController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductFormType;
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,15 +14,29 @@ class CatalogController extends AbstractController
     /**
      * @Route("/catalog", name="catalog")
      */
-    public function view(ProductRepository $productRepository)
+    public function view(ProductRepository $productRepository, Request $request)
     {
         $listProducts = $productRepository->findAll();
         $productByID = $productRepository->findOneBy(['id' => 1]);
 
+        $product = new Product();
+
+        $form = $this->createForm(ProductFormType::class, $product);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $product = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($product);
+            $manager->flush();
+        }
+
         return $this->render('/catalog/catalog.html.twig',
             [
                 'listProducts'  => $listProducts,
-                'productID'     => $productByID
+                'productID'     => $productByID,
+                'form'          => $form->createView(),
             ]
         );
     }
